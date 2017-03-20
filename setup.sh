ab09a76ea903c1ebc8f109f8771abc1fcc8e73f1
Garrett Davidson <gdavids@purdue.edu>
2017-03-19 23:11:26 -0400

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+echo "Setting up Mac..."
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+
+# Check for Homebrew and install if we don't have it
+if test ! $(which brew); then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update Homebrew recipes
+brew update
+
+# Install all our dependencies with bundle (See Brewfile)
+brew tap homebrew/bundle
+brew bundle
+
+# Make ZSH the default shell environment
+chsh -s $(which zsh)
+
+# Symlink dotfiles
+DIR=`pwd`
+ln -s $DIR/.zshrc $HOME/.zshrc
+ln -s $DIR/.gitconfig $HOME/.gitconfig
+ln -s $DIR/.gitignore $HOME/.gitignore
+ln -s $DIR/.wgetrc $HOME/.wgetrc
+ln -s $DIR/.emacs $HOME/.emacs
+
+# Setup Mac settings
+source .mac
