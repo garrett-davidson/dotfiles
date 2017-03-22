54be59e6a99698754aa5f70a97b58ebb62425b9d
Garrett Davidson <gdavids@purdue.edu>
2017-03-22 13:13:27 -0400

    Check if on a Mac before doing Mac specific things

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "Setting up Mac..."
+echo "Setting up..."
 
 # Ask for the administrator password upfront
 sudo -v
@@ -8,18 +8,24 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+if [ `uname -s` == "Darwin" ]; then
+    echo "Mac settings"
 
-# Check for Homebrew and install if we don't have it
-if test ! $(which brew); then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    # Check for Homebrew and install if we don't have it
+    if test ! $(which brew); then
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+
+    # Update Homebrew recipes
+    brew update
+
+    # Install all our dependencies with bundle (See Brewfile)
+    brew tap homebrew/bundle
+    brew bundle
+
+    # Setup Mac settings
+    source .mac
 fi
-
-# Update Homebrew recipes
-brew update
-
-# Install all our dependencies with bundle (See Brewfile)
-brew tap homebrew/bundle
-brew bundle
 
 # Make ZSH the default shell environment
 chsh -s $(which zsh)
@@ -31,6 +37,3 @@ ln -s $DIR/.gitconfig $HOME/.gitconfig
 ln -s $DIR/.gitignore $HOME/.gitignore
 ln -s $DIR/.wgetrc $HOME/.wgetrc
 ln -s $DIR/.emacs $HOME/.emacs
-
-# Setup Mac settings
-source .mac
