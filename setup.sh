a8f25c3379d67d29afd0618df2edb5d8e641cbab
Garrett Davidson <davidson.garrettm@gmail.com>
2018-04-17 20:53:20 -0400

    Install oh my zsh in setup.sh

--- setup.sh
+++ setup.sh
@@ -37,7 +37,11 @@ elif [ $KERNEL == "Linux" ]; then
 fi
 
 # Make ZSH the default shell environment
+sudo sh -c "echo $(which zsh) >> /etc/shells"
 chsh -s $(which zsh)
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Symlink dotfiles
 DIR=`pwd`
