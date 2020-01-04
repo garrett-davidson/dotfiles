acb64a17df622f04bad31dbcfbb8c9730c190c49
Garrett Davidson <davidson.garrettm@gmail.com>
2020-01-04 12:28:48 -0800

    Migrate emacs config to org

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -4,24 +4,33 @@
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
-(require 'better-defaults)
+;; Check for and (possibly) install use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
-(add-hook 'before-save-hook 'whitespace-cleanup)
+(org-babel-load-file "~/.emacs.d/configuration.org")
 
+;(use-package better-defaults)
+
+;(setq package-list '(better-defaults whitespace-cleanup smartparens))
+
+;(add-hook 'before-save-hook 'whitespace-cleanup)
+
+;(load "~/.emacs.d/custom/general.el")
+;(load "~/.emacs.d/custom/org.el")
+;(load "~/.emacs.d/custom/auto-complete.el")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (enh-ruby-mode smartparens better-defaults))))
+ '(package-selected-packages
+   (quote
+    (ws-butler use-package srcery-theme magit git-gutter+ flycheck better-defaults auto-compile))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-(load "~/.emacs.d/custom/general.el")
-(load "~/.emacs.d/custom/org.el")
-(load "~/.emacs.d/custom/auto-complete.el")
-(load "~/.emacs.d/custom/ruby.el")
